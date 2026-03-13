70c1e276b23f5cb01eebdbe97b1a6d7656b5417a
fazlu <fazlu@DESKTOP-AKH8F7G>
2026-03-13 19:59:46 +0530

    Add project files.

---
+++ BestPractice/ILogger.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestPractice
+{
+    public class ILogger
+    {
+        private static ILogger _instance;
+
+        public static ILogger GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new ILogger();
+            }
+            return _instance;
+        }
+        static string logMessage()
+        {
+          return "This is a log message.";
+        }
+        static ILogger testt
+        {
+            get; set;
+        }
+        private ILogger()
+        {
+            // Private constructor to prevent instantiation
+        }
+
+        public static ILogger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ILogger();
+                }
+                return _instance;
+            }
+        }
+
+        public void Logs(string message)
+        {
+            Console.WriteLine($"Log: {message}");
+        }
+
+        public void testlog()
+        {
+            Console.WriteLine("This is a test log message.");
+        }
+   
+         static void logMessage(string message)
+        {
+            ILogger.GetInstance().Logs(message);
+        }
+
+
+        //static void Main(string[] args)
+        //{
+        //    ILogger logger = ILogger.GetInstance();
+        //    logger.Logs("This is a log message.");
+        //    logger.testlog();
+        //    ILogger.testt = ILogger.GetInstance();
+        //     ILogger.testt.Logs("This is a log message from testt.");
+        //     ILogger.testt.testlog();
+        //    ILogger.logMessage();
+        //    logger.Logs(ILogger.logMessage());
+
+        //}
+    }
+}
---
+++ BestPractice/Startup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestPractice
+{
+    public class Startup
+    {
+        public delegate void Notify();
+        class Process //Process class that raises an event
+        {
+            public event Notify ProcessCompleted;
+
+            public void StartProcess()
+            {
+                Console.WriteLine("Process Started");
+
+                // Raise event
+                ProcessCompleted?.Invoke();
+            }
+            
+        }
+        public static class Logger
+        {
+            public static void Log(string message)
+            {
+                Console.WriteLine($"Log: {message}");
+            }
+        }
+        public static class DataAccess
+        {
+            // Static constructor to initialize the DataAccess class
+            static DataAccess()
+            {
+                Logger.Log("Data Access Layer Initialized");
+            }
+
+            public static void GetData()
+            {
+                Logger.Log("Fetching data from the database...");
+                // Simulate data fetching
+                System.Threading.Thread.Sleep(1000);
+                Logger.Log("Data fetched successfully.");
+            }
+        }
+
+        private static int add(int a, int b)
+        {
+            return a + b;
+        }
+        private static int mul(int a, int b) {
+            return a * b;
+        }
+
+        private delegate int Operating(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Operating addoperating = add;
+            Operating muloperating = mul;
+            Console.WriteLine("Addition: " + addoperating(5, 5));
+            Console.WriteLine("Multiplication: " + muloperating(5, 5));
+
+            Operating opradd = new Operating(add);
+            Operating oprmul = new Operating(mul);
+            Console.WriteLine("Addition: " + opradd(10, 10));
+            Console.WriteLine("Multiplication: " + oprmul(10, 10));
+
+
+
+            DataAccess.GetData();
+        }
+
+
+            //    static void OnProcessCompleted()
+            //    {
+            //        Console.WriteLine("Process Completed!");
+            //    }
+            //    Process p = new Process();
+            //    p.ProcessCompleted += OnProcessCompleted;
+            //    p.StartProcess();
+
+            //    Logger.Log("This is a log message.");
+            //    ILogger.Instance.testlog();
+            //    ILogger.Instance.Logs("This is another log message.");
+
+            //}
+
+
+
+        }
+
+}
